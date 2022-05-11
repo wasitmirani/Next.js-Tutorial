3480aa4946af5084788397f7145e3edade44ce59
Wasit Mirani <44178287+wasitmirani@users.noreply.github.com>
2022-05-11 07:18:28 -0600

    update

---
+++ components/navbar.js
@@ -0,0 +1,18 @@
+const NavBar= () => {
+
+    return (
+      <div >
+          <nav>
+                <div className="logo">
+                    <div>
+                        <h2>Logo</h2>
+                    </div>
+                    <a>Home</a>
+                    <a>About</a>
+                    <a>Contact</a>
+                </div>  
+          </nav>
+
+      </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import styles from "../styles/Home.module.css";
 export default function Home(){
   return (
     <div className={styles.container}>
+      
       <h1>Home</h1>
       </div>
     );
